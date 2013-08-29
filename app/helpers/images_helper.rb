06d09277f9c7ad17991a0be8845512cb4b4e8c7a
Andreas <andreas.westh@gmail.com>
2013-08-29 14:02:14 +0200

    handling keywords, dato and persons, correct

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,9 +1,13 @@
+# encoding: utf-8
 module ImagesHelper
 
   def create_image(filename)
     doc = REXML::Document.new File.new(filename)
     #logger.debug  doc.root.size
     @image = Image.new()
+    list_of_persons = Array.new
+    list_of_keywords = Array.new
+
     doc.root.elements.each('field/value') do |element|
       if element.parent.attributes['name'] == 'record_id'
         @image.record_id = element.text
@@ -40,8 +44,13 @@ module ImagesHelper
         end
       end
       if element.parent.attributes['name'] == 'Time'
+        @image.date_txt = element.text
+      end
+
+      if element.parent.attributes['name'] == 'År'
         @image.date_start = element.text
       end
+
       if element.parent.attributes['name'] == 'Asset Reference'
         @image.path_to_image = element.text
         # http://www.kb.dk/imageService/w450/online_master_arkiv_11/non-archival/Images/BILLED/2011/apr/DH_kvart/DH008765.jpg
@@ -54,18 +63,38 @@ module ImagesHelper
           @image.description = @image.description + '.' + element.text
         end
       end
-      if element.parent.attributes['name'] == 'Person'
-        if @image.description.nil? || @image.description.empty?
-          @image.description = element.text
-        else
-          @image.description = @image.description + 'Person: ' + element.text
-        end
+      #if element.parent.attributes['name'] == 'Person'
+      #  if @image.description.nil? || @image.description.empty?
+      #    @image.description = element.text
+      #  else
+      #    @image.description = @image.description + 'Person: ' + element.text
+      #  end
+      #end
+      if element.parent.attributes['name'] == 'Emneord'
+        list_of_keywords.push(element.text)
       end
+
+
+
       if element.parent.attributes['name'] == 'Person'
-        if @image.person.nil? || @image.person.empty?
-          @image.person = [element.text]
+        logger.debug("PERSON #{element.text}" )
+        if @image.person.nil? || @image.person.empty? || @image.person.length < 1
+          #@image.person = Array.new
+          #arra = ["12", "34"]
+
+          #arra.push(element.text)
+         # @image.person.push(element.text)
+          @image.person = element.text
+          list_of_persons.push(element.text)
+
+
         else
-          @image.person << element.text
+
+          #arra.push(element.text)
+          @image.person = (@image.person + ";" + element.text)
+          list_of_persons.push(element.text)
+          # @image.person.push( element.text)
+
         end
       end
       if element.parent.attributes['name'] == 'Copyright'
@@ -73,6 +102,14 @@ module ImagesHelper
       end
     end
 
+    #splitting on , for each person
+    #if !@image.person.nil? && !@image.person.empty? && @image.person.length > 0
+    #  @image.person = @image.person.split(';')
+    #end
+    @image.person = list_of_persons
+    logger.debug ("         list_of_persons: #{list_of_persons}")
+    @image.keywords = list_of_keywords
+    logger.debug ("         list_of_keywords: #{list_of_keywords}")
     @image
   end
 end
