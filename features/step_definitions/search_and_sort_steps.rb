7ee76d7c6570503e912bebb134de674e7cfe33f0
Blimey74 <jatr@kb.dk>
2013-08-30 15:39:29 +0200

    Added ominauth mock to cucumber tests, and using some of the more advanced cucumber constructs for creating tests, background, scenario outlines, etc

--- features/step_definitions/search_and_sort_steps.rb
+++ features/step_definitions/search_and_sort_steps.rb
@@ -1,14 +1,24 @@
 # -*- encoding : utf-8 -*-
 require 'capybara/rspec'
 
+Given(/^I am signed in with provider "([^"]*)"$/) do |arg|
+  visit new_user_session_path
+  page.should have_content 'Indlæs alle billeder'
+end
+
+When(/^test data is available$/) do
+  click_link 'Indlæs alle billeder'
+end
+
 Given(/^I am at the home page for Bifrost\-Billeder$/) do
   visit root_url
 end
 
+=begin
 When(/^I search for 'Carreby'$/) do
   fill_in 'q', :with => 'Carrebye'
   click_button 'search'
-end
+=end
 
 When(/^I sort by title$/) do
   click_link 'Titel'
@@ -26,6 +36,7 @@ When(/^I should get 'Vald Larsen' as the last result$/) do
   page.has_xpath?('//*[@id="documents"]/div/div/div[2]/dl/dd[1]').should == true
 end
 
+=begin
 When(/^I have selected 'Titel' search$/) do
   select 'Titel', :from => 'search_field'
 end
@@ -50,8 +61,17 @@ end
 
 Then(/^I should get (\d+) search result$/) do |arg|
   page.should have_content 'of ' + arg.to_s
-end
+=end
 
 Then(/^I should get (\d+) search results$/) do |arg|
   page.should have_content 'of ' + arg.to_s
+end
+
+When(/^I have selected (.*) search$/) do |metadata_field_name|
+  select metadata_field_name, :from => 'search_field'
+end
+
+When(/^I search for (.*)$/) do |search_term|
+  fill_in 'q', :with => search_term
+  click_button 'search'
 end
